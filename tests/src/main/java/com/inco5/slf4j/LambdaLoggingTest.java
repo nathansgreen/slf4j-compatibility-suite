46007f4e67bade0b0413e73bf7a367b2ea5db89c
Nathan Green <nathansgreen@users.noreply.github.com>
2015-10-22 22:43:34 -0500

    Deliberately induced test failures.

--- tests/src/main/java/com/inco5/slf4j/LambdaLoggingTest.java
+++ tests/src/main/java/com/inco5/slf4j/LambdaLoggingTest.java
@@ -24,6 +24,13 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
+import static java.nio.charset.StandardCharsets.US_ASCII;
+import static org.junit.Assert.assertEquals;
+
 /**
  * Test use of lambda-based logging methods against legacy SLF4J implementations
  * in order to ensure the default methods work correctly
@@ -31,10 +38,22 @@ import org.slf4j.LoggerFactory;
 public class LambdaLoggingTest {
 
     @Test
-    public void testError() {
-        // todo: capture system out
-        Logger logger = LoggerFactory.getLogger(LambdaLoggingTest.class);
-        logger.error("one {} two {} three {}", () -> new Object[]{"uno", "dos", "tres"});
+    public void testError() throws Exception {
+        final ByteArrayOutputStream out = new ByteArrayOutputStream(4096);
+        synchronized (System.err) {
+            final PrintStream stderr = System.err;
+            try {
+                System.setErr(new PrintStream(out, true, "US-ASCII"));
+                Logger logger = LoggerFactory.getLogger(LambdaLoggingTest.class);
+                System.out.println(logger);
+                logger.error("one {} two {} three {}", () -> new Object[]{"uno", "dos", "tres"});
+                String result = new String(out.toByteArray(), US_ASCII);
+                assertEquals("one uno two dos three tres", result);
+                System.out.println(result);
+            } finally {
+                System.setErr(stderr);
+            }
+        }
     }
 
 }
